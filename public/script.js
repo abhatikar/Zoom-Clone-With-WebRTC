7f13999cb3e01c7bbb22e4a0c942e488ffd03707
Abhijeet Bhatikar <abhijeet4jobs@gmail.com>
2021-04-27 16:14:08 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -7,7 +7,7 @@ const videoGrid = document.getElementById('video-grid')
 
 const myPeer = new Peer('someid', {
   secure: true, 
-  host: 'mysmartbell.herokuapp.com/', 
+  host: 'bellpeerjs.herokuapp.com/', 
   port: 443,
 })
 
